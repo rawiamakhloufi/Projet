fbf2264610c8067fa2e2c4ca57e92a2447541410
rawiamakhloufi <98602119+rawiamakhloufi@users.noreply.github.com>
2022-02-28 20:19:19 +0100

    Add files via upload

---
+++ assets/js/uni.js
@@ -0,0 +1,20 @@
+{
+"type": "FeatureCollection",
+"name": "uni",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Field1": "Universités", "Field2": "X", "Field3": "Y" }, "geometry": null },
+{ "type": "Feature", "properties": { "Field1": "Université Mohamed V - Rabat", "Field2": "33.987463529103735", "Field3": "-6.859473172902889" }, "geometry": { "type": "Point", "coordinates": [ -6.020529951773739, 32.212096165852543 ] } },
+{ "type": "Feature", "properties": { "Field1": "Université Hassan II - Casablanca", "Field2": "33.53975755477347", "Field3": "-7.657198558108755" }, "geometry": { "type": "Point", "coordinates": [ -6.468235926104001, 31.414370780646678 ] } },
+{ "type": "Feature", "properties": { "Field1": "Université Sidi Mohammed Ben Abdellah - Fès", "Field2": "34.0362824045135", "Field3": "-4.999699303032039" }, "geometry": { "type": "Point", "coordinates": [ -5.971711076363974, 34.071870035723393 ] } },
+{ "type": "Feature", "properties": { "Field1": "Université Mohammed Premier - Oujda", "Field2": "34.65025816955121", "Field3": " -1.8989171386037684" }, "geometry": { "type": "Point", "coordinates": [ -5.357735311326266, 37.17265220015166 ] } },
+{ "type": "Feature", "properties": { "Field1": "Université Cadi Ayyad - Marrakech", "Field2": "31.648617146811674", "Field3": " -7.994366136044333" }, "geometry": { "type": "Point", "coordinates": [ -8.3593763340658, 31.077203202711097 ] } },
+{ "type": "Feature", "properties": { "Field1": "Université Moulay Smail - Meknès", "Field2": "33.85619116744802", "Field3": " -5.574112053173671" }, "geometry": { "type": "Point", "coordinates": [ -6.151802313429457, 33.49745728558176 ] } },
+{ "type": "Feature", "properties": { "Field1": "Université Abdelmalek Essaadi - Tétouan", "Field2": "35.56070944307842", "Field3": " -5.364063747582346" }, "geometry": { "type": "Point", "coordinates": [ -4.447284037799051, 33.707505591173089 ] } },
+{ "type": "Feature", "properties": { "Field1": "Université Chouaib Doukkali – El jadida", "Field2": "33.225446070358835", "Field3": " -8.486178221282328" }, "geometry": { "type": "Point", "coordinates": [ -6.782547410518639, 30.585391117473101 ] } },
+{ "type": "Feature", "properties": { "Field1": "Université Ibn Tofail – Kénitra", "Field2": "34.24623932246613", "Field3": " -6.584705813522992" }, "geometry": { "type": "Point", "coordinates": [ -5.761754158411343, 32.486863525232437 ] } },
+{ "type": "Feature", "properties": { "Field1": "Université Ibn Zohr - Agadir", "Field2": "30.410620414952465", "Field3": " -9.543078805393032" }, "geometry": { "type": "Point", "coordinates": [ -9.597373065925009, 29.528490533362401 ] } },
+{ "type": "Feature", "properties": { "Field1": "Université Hassan Premier - Settat", "Field2": "33.02501833708152", "Field3": " -7.615380088135794" }, "geometry": { "type": "Point", "coordinates": [ -6.982975143795954, 31.456189250619637 ] } },
+{ "type": "Feature", "properties": { "Field1": "Université Sultan Moulay Slimane – Béni Mellal", "Field2": "32.33604944771157", "Field3": " -6.368565851928994" }, "geometry": { "type": "Point", "coordinates": [ -7.671944033165907, 32.703003486826439 ] } }
+]
+}
